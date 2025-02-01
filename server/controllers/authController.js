6404a8f07df052405d3d8f1c49612939b5b6274a
Sidharth Bhasin <sid21bhasin@gmail.com>
2025-02-01 14:29:40 +0530

    feat: enhance login functionality; set cookie domain based on environment

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -84,6 +84,7 @@ module.exports.loginUser = async (req, res) => {
                     secure: process.env.NODE_ENV === 'production',
                     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
                     maxAge: 7 * 24 * 60 * 60 * 1000,
+                    domain: process.env.NODE_ENV === 'production' ? '.vercel.app' : 'localhost'
                 });
                 res.json({
                     user,
